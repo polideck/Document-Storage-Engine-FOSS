f12b25b5280a9bb194d9108117fa7eaf8c6f78be
ajgosset <ajgosset@asu.edu>
2021-10-07 17:51:38 -0700

    Front End File Upload

---
+++ front-end/recieveInput.js
@@ -0,0 +1,13 @@
+
+window.onload = function(){
+  const inputElement = document.getElementById("submitButton");
+  inputElement.addEventListener("click", handleFile, false);
+
+}
+
+function handleFile() {
+  console.log('test')
+  fileUpload = document.getElementById("inputForm");
+  const file = fileUpload.files[0];
+  console.log(file);
+}

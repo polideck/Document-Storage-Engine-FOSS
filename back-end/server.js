cfc19b4e6549d9c0656c0d46e2ee86638240b40a
cmajorb <cmajorb@gmail.com>
2021-11-20 15:12:53 -0700

    Upload to S3 bucket

--- back-end/server.js
+++ back-end/server.js
@@ -3,15 +3,29 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 var path = require("path");
+const AWS = require('aws-sdk');
+var multer  =   require('multer');
+var multerS3 = require('multer-s3');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+const ID = '';
+const SECRET = '';
+const BUCKET_NAME = 'polideck-bucket-1';
+
 
 // App
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: false}));
+
+const s3 = new AWS.S3({
+  accessKeyId: 'AKIA5JKZND6QG7QZMIHZ',
+  secretAccessKey: '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',
+  region: 'us-west-1'
+})
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'./public/index.html'));
@@ -21,9 +35,22 @@ app.get('/fileupload', (req, res) => {
   res.sendFile(path.join(__dirname,'./public/fileupload.html'));
 });
 
-app.post('/add', function(req,res){
-  res.send("Received document named " + req.body.filename);
+var upload = multer({
+  storage: multerS3({
+      s3: s3,
+      bucket: BUCKET_NAME,
+      key: function (req, file, cb) {
+          console.log(file);
+          cb(null, file.originalname); //use Date.now() for unique file keys
+      }
+  })
+});
+
+app.post('/add', upload.array('upl',1), function (req, res, next) {
+  res.send("Uploaded!");
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+

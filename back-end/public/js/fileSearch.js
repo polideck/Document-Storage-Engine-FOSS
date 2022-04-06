fd7667def90bb84c0e3e2bd0390d34cf151640aa
ajgosset <ajgosset@asu.edu>
2022-04-05 18:36:26 -0700

    don't show data

--- back-end/public/js/fileSearch.js
+++ back-end/public/js/fileSearch.js
@@ -1,6 +1,6 @@
 
 var url = "http://localhost:3000/api/authenticate-token";
-
+var authenticated = false;
 var bearer = 'Bearer ' + localStorage.getItem('bearer')
 console.log('bearer3')
 console.log(bearer);
@@ -25,228 +25,232 @@ fetch(url, {
 
             if(res.status != 200)
                 window.location.replace('/login')
+            else
+                authenticated = true;
         }
     );
 
 
+if(authenticated){
+    $(document).ready(function() {
+    $('table').bootstrapTable({
+        data: mydata
+    });
 
-$(document).ready(function() {
-  $('table').bootstrapTable({
-      data: mydata
-  });
-
-  $("#myInput").on("keyup", function() {
-      var value = $(this).val().toLowerCase();
-      $("#blockchain-table tr").filter('tr:not(:first)').filter(function() {
-          $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-      });
-  });
-});
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#blockchain-table tr").filter('tr:not(:first)').filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+    });
 
 
-// JSON data
-var mydata = [{
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    // JSON data
+    var mydata = [{
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-  {
-      'Name': 'File 1 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
-  },
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    {
+        'Name': 'File 1 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'AGHsdcafSDAF254235fWFweqfWaFEr%EWf43%6435435erWF'
+    },
 
-  {
-      'Name': 'File 2 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
-  },
+    {
+        'Name': 'File 2 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'BFADKVJSDFV13weafjf1345GYT%3tfWDFVwerf'
+    },
 
-  {
-      'Name': 'File 3 Name',
-      'DateAdded': Date.now(),
-      'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
-  },
-];
+    {
+        'Name': 'File 3 Name',
+        'DateAdded': Date.now(),
+        'CreatedBy': 'asdfsdfFDFCdc%3tfdsafR4'
+    },
+    ];
+
+}

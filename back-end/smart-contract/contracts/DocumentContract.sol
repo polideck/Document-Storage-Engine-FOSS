dd8af852bd6beac3a341d5fc09852119b731788b
ajgosset <ajgosset@asu.edu>
2022-04-13 20:29:03 -0700

    Document And Master Contract start

--- back-end/smart-contract/contracts/DocumentContract.sol
+++ back-end/smart-contract/contracts/DocumentContract.sol
@@ -1,41 +1,33 @@
-pragma solidity >=0.4.22 <0.9.0;
-
-/* How Master Contract Moves:
-    Owner Address (Used to index for each user)
-        List of Document Contracts (Appends new Document Contract for every upload based on IPFS hash) (Used to search/view all documents on main search page)
-            Document Contract (Has its own contract address and name for each document on blockchain)
-                Document Revisions/Versions w/ Hashes (Used to download from IPFS based on hash stored)
-                
- */
-contract MasterContract {
-    //Needs to take in public owner address 
-
-    //Using owner address grab List of Document Contracts
-
-    //Document Contracts
-    /*List of Document Contracts:
-    {
-        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT},
-        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT},
-        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT}
-    } */
-
-
-
-
-    //Function needed to take IPFS hash and make new Document Contract (or add new revision) (FOR UPLOAD)
-
-    //Function needed to get all or specifically searched document contracts from specified owner (FOR SEARCH/VIEWING)
-
-    //Function needed to download most recent document revision (actually getting hash) (FOR DOWNLOAD)
-
-    //Function needed to grab most recent document revision to delete (FOR DELETE)
-
-}
-
+pragma solidity ^0.8.13;
 
 contract DocumentContract {
+    struct RevisionDocument{
+        string ipfsHash;
+    }
+
     //List Document Revisions and their respective hashes
     //Want an array of document versions
+    string public ipfsHash;
+    address[] public ownerAddresses;
 
+    string public documentName;
+
+    constructor(string memory _ipfsHash, address[] memory _ownerAddresses, string memory _documentName){
+        ipfsHash = _ipfsHash;
+        ownerAddresses = _ownerAddresses;
+        documentName = _documentName;
+    }
+    
+    //Hashes to previous versions of document
+    RevisionDocument[] public revisions;
+
+    function AppendRevision(string memory newIpfsHash) public{
+        revisions.push(RevisionDocument({ipfsHash:ipfsHash}));
+
+        ipfsHash = newIpfsHash;
+    }
+
+    function GetOwners() public view returns(address[] memory){
+        return ownerAddresses;
+    }
 }
---
+++ back-end/smart-contract/contracts/MasterContract.sol
@@ -0,0 +1,58 @@
+/* How Master Contract Moves:
+    Owner Address (Used to index for each user)
+        List of Document Contracts (Appends new Document Contract for every upload based on IPFS hash) (Used to search/view all documents on main search page)
+            Document Contract (Has its own contract address and name for each document on blockchain)
+                Document Revisions/Versions w/ Hashes (Used to download from IPFS based on hash stored)
+                
+ */
+import "./DocumentContract.sol";
+
+contract MasterContract {
+    // struct owner{
+    //     address ownerAddress;
+    //     address[] documentContracts;
+    // }
+
+    address constant private serverAddress = 0xE0f5206BBD039e7b0592d8918820024e2a7437b9;
+    modifier onlyOwner(){
+        require(msg.sender == serverAddress);
+        _;
+    }
+
+    //Needs to take in public owner address 
+    //owner[] public owners;
+
+    //ownerAddress => documentContracts
+
+    mapping(address => address[]) public owners;
+
+    //Using owner address grab List of Document Contracts
+    function getListOfDocuments(address owner) public view returns(address[] memory){
+        return owners[owner];
+    }
+
+    //Document Contracts
+    /*List of Document Contracts:
+    {
+        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT},
+        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT},
+        {DOC_CONTRACT_ADDRESS: NAME_OF_DOCUMENT}
+    } */
+
+    //Function needed to take IPFS hash and make new Document Contract (FOR UPLOAD)
+
+    //Note: Add in onlyOwner modifier
+    function uploadDocument(string memory ipfsHash, address ownerAddress, string memory documentName) public{
+        
+        DocumentContract docContract = new DocumentContract(ipfsHash, ownerAddress, documentName);
+    }
+
+    //Function for revision
+
+    //Function needed to get all or specifically searched document contracts from specified owner (FOR SEARCH/VIEWING)
+
+    //Function needed to download most recent document revision (actually getting hash) (FOR DOWNLOAD)
+
+    //Function needed to grab most recent document revision to delete (FOR DELETE)
+
+}

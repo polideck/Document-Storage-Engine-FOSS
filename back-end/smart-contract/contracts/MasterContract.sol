8262cfed8a8dc4935d77f3adc6111bd1e60cf631
ajgosset <ajgosset@asu.edu>
2022-04-15 18:10:36 -0700

    document contract change

--- back-end/smart-contract/contracts/MasterContract.sol
+++ back-end/smart-contract/contracts/MasterContract.sol
@@ -17,35 +17,35 @@ contract MasterContract {
     }
 
     //ownerAddress => documentContracts
-    mapping(address => address[]) public owners;
+    mapping(address => DocumentContract[]) public owners;
 
     //Using owner address grab List of Document Contracts
-    function getListOfDocuments(address owner) public view returns(address[] memory){
+    function getListOfDocuments(address owner) public view returns(DocumentContract[] memory){
         return owners[owner];
     }
 
     //Note: Add in onlyOwner modifier
     function uploadDocument(string memory ipfsHash, address ownerAddress, string memory documentName) public{
-        owners[ownerAddress].push(address(new DocumentContract(ipfsHash, ownerAddress, documentName)));
+        owners[ownerAddress].push(new DocumentContract(ipfsHash, ownerAddress, documentName));
     }
 
     //Function needed to get all or specifically searched document contracts from specified owner (FOR SEARCH/VIEWING)
     //Note: Add in onlyOwner modifier
-    function getContractsByOwner(address ownerAddress) public view returns(address[] memory){
+    function getContractsByOwner(address ownerAddress) public view returns(DocumentContract[] memory){
         return owners[ownerAddress];
     }
 
     //Note: Add in onlyOwner modifier
-    function deleteOwnerFromDocument(address owner, address document) public{
-        address[] memory documentAddresses = owners[owner];
-        for(uint i = 0; i < documentAddresses.length; i++){
-            if(documentAddresses[i] == document){
-                documentAddresses[i] = documentAddresses[documentAddresses.length - 1];
-                delete documentAddresses[documentAddresses.length - 1];
+    function deleteOwnerFromDocument(address owner, DocumentContract document) public{
+        DocumentContract[] memory documents = owners[owner];
+        for(uint i = 0; i < documents.length; i++){
+            if(documents[i] == document){
+                documents[i] = documents[documents.length - 1];
+                delete documents[documents.length - 1];
                 break;
             }
         }
 
-        owners[owner] = documentAddresses;
+        owners[owner] = documents;
     }
 }

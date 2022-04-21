8d6f9141ef290a589fb0a8ada8b35f3ea55245fa
ajgosset <ajgosset@asu.edu>
2022-04-20 18:50:17 -0700

    Can view address from get list of documents

--- back-end/smart-contract/contracts/MasterContract.sol
+++ back-end/smart-contract/contracts/MasterContract.sol
@@ -18,10 +18,10 @@ contract MasterContract {
         for(uint i = 0; i < contracts.length; i++){
             string[2] memory hashAndDocument = contracts[i].getIpfsHashAndDocumentName();
             if(i != contracts.length - 1){
-                output = string(bytes.concat(bytes(output), '{ "Hash": "', bytes(hashAndDocument[0]), '", "Name": "', bytes(hashAndDocument[1]), '"}, '));
+                output = string(bytes.concat(bytes(output), '{ "Hash": "', bytes(hashAndDocument[0]), '", "Name": "', bytes(hashAndDocument[1]), '", "Address": "', bytes(toAsciiString(address(contracts[i]))), '"}, '));
             }
             else{
-                output = string(bytes.concat(bytes(output), '{ "Hash": "', bytes(hashAndDocument[0]), '", "Name": "', bytes(hashAndDocument[1]), '"} '));
+                output = string(bytes.concat(bytes(output), '{ "Hash": "', bytes(hashAndDocument[0]), '", "Name": "', bytes(hashAndDocument[1]), '", "Address": "' , bytes(toAsciiString(address(contracts[i]))), '"} '));
             }
         }
 
@@ -60,5 +60,44 @@ contract MasterContract {
 
         //No matches
         return address(0x0);
+    }
+
+    function _addressToString(address addr) private pure returns (string memory){
+        bytes memory addressBytes = abi.encodePacked(addr);
+
+        bytes memory stringBytes = new bytes(42);
+
+        stringBytes[0] = '0';
+        stringBytes[1] = 'x';
+
+        for(uint i = 0; i< 20; i++){
+            uint8 leftValue = uint8(addressBytes[i])/16;
+            uint8 rightValue = uint8(addressBytes[i]) - 16 * leftValue;
+
+            bytes1 leftChar = leftValue < 10? bytes1(leftValue + 48) : bytes1(leftValue + 87);
+            bytes1 rightChar = rightValue < 10? bytes1(rightValue + 48) : bytes1(rightValue + 87);
+
+            stringBytes[2 * i + 3] = rightChar;
+            stringBytes[2 * i + 3] = leftChar;
+        }
+
+        return string(stringBytes);
+    }
+
+    function toAsciiString(address x) internal pure returns (string memory) {
+    bytes memory s = new bytes(40);
+    for (uint i = 0; i < 20; i++) {
+        bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+        bytes1 hi = bytes1(uint8(b) / 16);
+        bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+        s[2*i] = char(hi);
+        s[2*i+1] = char(lo);            
+    }
+    return string(s);
+}
+
+    function char(bytes1 b) internal pure returns (bytes1 c) {
+        if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+        else return bytes1(uint8(b) + 0x57);
     }
 }
